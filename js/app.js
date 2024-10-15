d368c3db6b988996089f7c908ad5fb4413c6ec70
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-15 14:47:28 +0200

    ADD: printing of numbers with variant for multiples of 3 and 5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 // ciclo for
 for (let i = 0; i < 100; i++) {
-    console.log(i + 1)
+    if ((((i +1) % 3) === 0) && (((i +1) % 5) === 0)) {
+        console.log("FizzBuzz")
+    } else if (((i +1) % 3) === 0) {
+        console.log("Fizz")
+    } else if (((i +1) % 5) === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i + 1)
+    }
 }
